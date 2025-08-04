78f5eb41c6930d5a0e34a6901852108686f1a271
angelnext <contact@angelnext.dev>
2025-08-04 02:04:18 +0200

    fix(hosting): Add http server for hosting trickery

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { setListeners } from "./utils/listeners.js";
 import { log } from "./utils/logger.js";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
 const client = new Client({
 	intents: [
@@ -22,6 +24,10 @@ if (process.env.NODE_ENV === "development") {
 try {
 	await setListeners(client);
 	await client.login(TOKEN);
+
+	const app = new Hono();
+	app.get("/", (c) => c.text("Something something"));
+	serve(app);
 } catch (err) {
 	log.fatal("Error loading commands and/or starting client");
 	console.error(err);
